f1df5afb3b266e8ba0828cf13ea6da8692c7d298
Shijie Li <lishijie0602@gmail.com>
2017-08-03 22:32:03 -0400

    combination sum 3

--- leetcode/CombinationSumIII.h
+++ leetcode/CombinationSumIII.h
@@ -21,5 +21,30 @@ using namespace std;
 
 class CombinationSumIII {
   public:
+      vector<vector<int>> combinationSum3(int k, int n) {
+          vector<vector<int>> res;
+          vector<int> path;
+          dfs(k, n, 1, path, res);
+          return res;
+      }
   private:
+    void dfs(const int k,
+             int n,
+             int start,
+             vector<int>& path,
+             vector<vector<int> >& res)
+    {
+        if(n == 0 && path.size() == k) {
+            res.emplace_back(path);
+            return;
+        }
+        if(path.size() >= k) return;
+        for(int i = start; i <=9; ++i) {
+            int diff = n - i;
+            if(diff < 0) return;
+            path.push_back(i);
+            dfs(k, n-i, i+1, path, res);
+            path.pop_back();
+        }
+    }
 };
